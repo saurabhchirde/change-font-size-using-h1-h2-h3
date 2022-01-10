b66535115efee3846b3f094375ecc803a14fb81d
saurabhchirde <saurabh123.chirde@gmail.com>
2022-01-10 23:24:16 +0530

    fix: if-else to ternary operator

--- script.js
+++ script.js
@@ -4,27 +4,23 @@ const btnH2 = document.querySelector(".btnH2");
 const btnH3 = document.querySelector(".btnH3");
 const output = document.querySelector(".output");
 
-btnH1.addEventListener("click", function () {
+btnH1.addEventListener("click", () => {
   const input = str.value;
-  if (input !== "") {
-    output.innerHTML = "<h1>" + input + "</h1>";
-  } else {
-    output.innerText = "Enter some text";
-  }
+  input !== ""
+    ? (output.innerHTML = "<h1>" + input + "</h1>")
+    : (output.innerText = "Enter some text");
 });
+
 btnH2.addEventListener("click", () => {
   const input = str.value;
-  if (input !== "") {
-    output.innerHTML = "<h2>" + input + "</h2>";
-  } else {
-    output.innerText = "Enter some text";
-  }
+  input !== ""
+    ? (output.innerHTML = "<h2>" + input + "</h2>")
+    : (output.innerText = "Enter some text");
 });
+
 btnH3.addEventListener("click", () => {
   const input = str.value;
-  if (input !== "") {
-    output.innerHTML = "<h3>" + input + "</h3>";
-  } else {
-    output.innerText = "Enter some text";
-  }
+  input !== ""
+    ? (output.innerHTML = "<h3>" + input + "</h3>")
+    : (output.innerText = "Enter some text");
 });
